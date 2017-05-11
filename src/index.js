9c47cca7506737dacb4df89653f4df5c34a0ab8c
Andrew Piper <pipera88@mgail.com>
2017-05-11 11:58:21 -0700

    target change

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import ImageScroll from "./ImageScroll.html";
 
 var imagescroll = new ImageScroll({
-  target: document.querySelector("section.section1"),
+  target: document.querySelector("section div.imagescroll"),
   data: {
     transition: "slide",
   },
